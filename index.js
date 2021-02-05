b75ba3650f89a734eb840b94b3bc7a4192a2c905
Max <veyronmclord@gmail.com>
2021-02-05 12:33:59 +0600

    fixed some

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const readline = require('readline');
 
 
 let inputTxt = fs.readFileSync('input.txt').toString().toLowerCase();
@@ -13,50 +14,86 @@ const pat = patternsTxt.split(',');
 let firstFound = [];
 let secondFound = [];
 
+
 /* FOR FIRST SUBTASK */ 
-for (let i = 0; i < inp.length; i++) {
-  for (let j = 0; j < pat.length; j++) {
-    if (inp[i].includes(pat[j])) {
-      if (!firstFound.includes(inp[i])) {
-        firstFound.push(inp[i]);
+function firstTask(str1, str2) {
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i].includes(str2[j])) {
+        if (!firstFound.includes(str1[i])) {
+          firstFound.push(str1[i]);
+        }
       }
     }
   }
+  console.log(chalk.blueBright("\nFirst: "),firstFound.join(', '));
 }
-console.log(chalk.blueBright("\nFirst: "),firstFound.join(', '));
+
 
 
 /* FOR SECOND SUBTASK */ 
-for (let i = 0; i < inp.length; i++) {
-  for (let j = 0; j < pat.length; j++) {
-    if ((inp[i] == pat[j]) && (pat[j] !== undefined) && (inp[i].length == pat[j].length)) {
-      if(!secondFound.includes(inp[i])) {
-        secondFound.push(inp[i]);
+function secondTask(str1, str2) {
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if ((str1[i] == str2[j]) && (str2[j] !== undefined) && (str1[i].length == str2[j].length)) {
+        if(!secondFound.includes(str1[i])) {
+          secondFound.push(str1[i]);
+        }
       }
-    }
-  }  
+    }  
+  }
+  console.log(chalk.blueBright("\nSecond: "),secondFound.join(', '));
 }
-console.log(chalk.blueBright("\nSecond: "),secondFound.join(', '));
+
 
 
 /* FOR THIRD SUBTASK */ 
-function diffOneLetter(str1, str2) {
-  let dist = 0;
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
-  for (let i = 0, j = Math.max(str1.length, str2.length); i < j; i++) {
-    if (!str1[i] || !str2[i] || str1[i] !== str2[i]) {
-      dist++;
+function thirdTask(str1, str2) {
+  function diffOneLetter(dif1, dif2) {
+    let dist = 0;
+    dif1 = dif1.toLowerCase();
+    dif2 = dif2.toLowerCase();
+    for (let i = 0, j = Math.max(dif1.length, dif2.length); i < j; i++) {
+      if (!dif1[i] || !dif2[i] || dif1[i] !== dif2[i]) {
+        dist++;
+      }
     }
+    return dist;
   }
-  return dist;
+  
+  console.log(chalk.blueBright("\nThird: "))
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if (diffOneLetter(str1[i], str2[j]) <= 1) {
+        console.log(str1[i])
+      }
+    }  
+  }
 }
 
-console.log(chalk.blueBright("\nThird: "))
-for (let i = 0; i < inp.length; i++) {
-  for (let j = 0; j < pat.length; j++) {
-    if (diffOneLetter(inp[i], pat[j]) <= 1) {
-      console.log(inp[i])
-    }
-  }  
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(chalk.redBright('\nWhich mode u want to choose? (1, 2 or 3) '), (value) => {
+  switch (value) {
+    case "1":
+      firstTask(inp, pat);
+      break;
+    case "2":
+      secondTask(inp, pat);
+      break;
+    case "3":
+      thirdTask(inp, pat);
+      break;
+
+    default:
+      break;
+  }
+  rl.close();
+});
+
+// firstTask(inp, pat);
+// secondTask(inp, pat);
+// thirdTask(inp, pat);
